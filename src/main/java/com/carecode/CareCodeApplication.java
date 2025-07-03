29e40b73d21adb5ea491538e8ffd747f16a1b973
RosieOh <20172207@gm.hannam.ac.kr>
2025-07-03 10:54:26 +0900

    BUILD : Initial Commit

---
+++ src/main/java/com/carecode/CareCodeApplication.java
@@ -0,0 +1,13 @@
+package com.carecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carecode/CareCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
