6aaf542280b7930bccd3ed044ed25a3bb85cf1f4
RosieOh <dhxogns920@gmail.com>
2025-07-12 14:50:06 +0900

    CHORE : 비밀번호 암호화 빈등록 시 스프링부트 버전 오류로 인한 생성자 작성 방식 변경 #1

--- src/main/java/com/carecode/core/config/PasswordConfig.java
+++ src/main/java/com/carecode/core/config/PasswordConfig.java
@@ -1,11 +1,15 @@
 package com.carecode.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
-    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
