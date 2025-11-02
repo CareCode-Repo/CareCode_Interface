f47d06280258e6009ab30c357317df5fcf76687a
RosieOh <dhxogns920@gmail.com>
2025-11-02 18:19:59 +0900

    FEAT : Json - Map, HashMap 변환 유틸리티 컴포넌트 추가

---
+++ src/main/java/com/carecode/core/components/JsonMapComponent.java
@@ -0,0 +1,30 @@
+package com.carecode.core.components;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JsonMapComponent extends HashMap<String, Object> {
+
+    public JsonMapComponent() {
+        super();
+    }
+
+    public List<Map<String, Object>> getArrayListData(String key) {
+        if (key != null && containsKey(key))
+            return (List<Map<String, Object>>) get(key);
+        return null;
+    }
+
+    public Map<String, Object> getHashMapData(String key) {
+        if(key != null && containsKey(key))
+            return (Map<String, Object>) get(key);
+        return null;
+    }
+
+    public String getStringData(String key) {
+        if (key != null && containsKey(key))
+            return (String) get(key);
+        return null;
+    }
+}
