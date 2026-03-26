d4fea8eb87b4f44ca0475202ed5d562f47954899
RosieOh <dhxogns920@gmail.com>
2026-03-27 00:05:48 +0900

    CHORE : HospitalLikeRepository 정리
    주석/표현/경미한 구조를 정리했습니다. 동작 변경 없이 가독성과 일관성을 맞췄고, 변경량은 추가 0줄, 삭제 3줄입니다.

--- src/main/java/com/carecode/domain/health/repository/HospitalLikeRepository.java
+++ src/main/java/com/carecode/domain/health/repository/HospitalLikeRepository.java
@@ -6,10 +6,7 @@ import com.carecode.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HospitalLikeRepository extends JpaRepository<HospitalLike, Long> {
-    long countByHospital(Hospital hospital);
     long countByHospitalId(Long hospitalId);
-    boolean existsByHospitalAndUser(Hospital hospital, User user);
     boolean existsByHospitalIdAndUserId(Long hospitalId, Long userId);
-    void deleteByHospitalAndUser(Hospital hospital, User user);
     void deleteByHospitalIdAndUserId(Long hospitalId, Long userId);
 } 
